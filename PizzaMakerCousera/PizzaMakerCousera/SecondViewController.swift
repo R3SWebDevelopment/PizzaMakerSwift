52ef6b331a9a49159f28ea4194651ce787f35dd3
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2018-08-17 22:17:40 -0500

    Add third view controller

--- PizzaMakerCousera/PizzaMakerCousera/SecondViewController.swift
+++ PizzaMakerCousera/PizzaMakerCousera/SecondViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var crujiente: UIImageView!
+    @IBOutlet weak var delgada: UIImageView!
+    @IBOutlet weak var gruesa: UIImageView!
+    
+    var isCompleted = false
+    var value = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapCrujiente = UITapGestureRecognizer(target: self, action: #selector(SecondViewController.selectCrujiente))
+        let tapDelgada = UITapGestureRecognizer(target: self, action: #selector(SecondViewController.selectDelgada))
+        let tapGruesa = UITapGestureRecognizer(target: self, action: #selector(SecondViewController.selectGruesa))
+        
+        crujiente.addGestureRecognizer(tapCrujiente)
+        delgada.addGestureRecognizer(tapDelgada)
+        gruesa.addGestureRecognizer(tapGruesa)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,41 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func selectCrujiente(){
+        if value == "Crujiete"{
+            crujiente.alpha = 0.5
+            value = ""
+        }else{
+            crujiente.alpha = 1
+            value = "Crujiete"
+        }
+        delgada.alpha = 0.5
+        gruesa.alpha = 0.5
+    }
+    
+    @objc func selectDelgada(){
+        if value == "Delgada"{
+            delgada.alpha = 0.5
+            value = ""
+        }else{
+            delgada.alpha = 1
+            value = "Delgada"
+        }
+        crujiente.alpha = 0.5
+        gruesa.alpha = 0.5
+    }
+    
+    @objc func selectGruesa(){
+        if value == "Gruesa"{
+            gruesa.alpha = 0.5
+            value = ""
+        }else{
+            gruesa.alpha = 1
+            value = "Gruesa"
+        }
+        crujiente.alpha = 0.5
+        delgada.alpha = 0.5
+    }
+    
 }
 
---
+++ PizzaMakerCousera/PizzaMakerCousera/ThirdViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ThirdViewController.swift
+//  PizzaMakerCousera
+//
+//  Created by Ricardo Tercero Solis on 17/08/18.
+//  Copyright © 2018 R3S. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var sinQueso: UIImageView!
+    @IBOutlet weak var cheddar: UIImageView!
+    @IBOutlet weak var mozzarrella: UIImageView!
+    @IBOutlet weak var parmesano: UIImageView!
+    var isCompleted = false
+    var value = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let tapSinQueso = UITapGestureRecognizer(target: self, action: #selector(ThirdViewController.selectSinQueso))
+        let tapCheddar = UITapGestureRecognizer(target: self, action: #selector(ThirdViewController.selectCheddar))
+        let tapMozzarrella = UITapGestureRecognizer(target: self, action: #selector(ThirdViewController.selectMozzarrella))
+        let tapParmesano = UITapGestureRecognizer(target: self, action: #selector(ThirdViewController.selectParmesano))
+        
+        sinQueso.addGestureRecognizer(tapSinQueso)
+        cheddar.addGestureRecognizer(tapCheddar)
+        mozzarrella.addGestureRecognizer(tapMozzarrella)
+        parmesano.addGestureRecognizer(tapParmesano)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @objc func selectSinQueso(){
+        if value == "Sin Queso"{
+            sinQueso.alpha = 0.5
+            value = ""
+        }else{
+            sinQueso.alpha = 1
+            value = "Sin Queso"
+        }
+        cheddar.alpha = 0.5
+        mozzarrella.alpha = 0.5
+        parmesano.alpha = 0.5
+    }
+    
+    @objc func selectCheddar(){
+        if value == "Cheddar"{
+            cheddar.alpha = 0.5
+            value = ""
+        }else{
+            cheddar.alpha = 1
+            value = "Sin Queso"
+        }
+        sinQueso.alpha = 0.5
+        mozzarrella.alpha = 0.5
+        parmesano.alpha = 0.5
+    }
+    
+    @objc func selectMozzarrella(){
+        if value == "Mozzarrella"{
+            mozzarrella.alpha = 0.5
+            value = ""
+        }else{
+            mozzarrella.alpha = 1
+            value = "Mozzarrella"
+        }
+        sinQueso.alpha = 0.5
+        cheddar.alpha = 0.5
+        parmesano.alpha = 0.5
+    }
+    
+    @objc func selectParmesano(){
+        if value == "Parmesano"{
+            parmesano.alpha = 0.5
+            value = ""
+        }else{
+            parmesano.alpha = 1
+            value = "Parmesano"
+        }
+        sinQueso.alpha = 0.5
+        cheddar.alpha = 0.5
+        mozzarrella.alpha = 0.5
+    }
+
+}
